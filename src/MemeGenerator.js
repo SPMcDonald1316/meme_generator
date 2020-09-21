6f2c772cc9ab68155c05d4b2e1c8052bc1072b2f
Sean McDonald <spmcdonald1316@gmail.com>
2020-09-21 18:56:35 -0500

    add initial state

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,7 +3,14 @@ import React from 'react'
 class MemeGenerator extends React.Component {
   constructor() {
     super()
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImgSrc: "http://i.imgflip.com/1bij.jpg"
+    }
   }
+
+  
 
   render() {
     return <h1>MEME GENERATOR</h1>
