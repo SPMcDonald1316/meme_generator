da7c60adcb1a1164ef57e4e648fc2918050f2371
Sean McDonald <spmcdonald1316@gmail.com>
2020-09-21 18:25:58 -0500

    add styling, outline components

---
+++ src/MemeGenerator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return <h1></h1>
+  }
+}
+
+export default MemeGenerator
