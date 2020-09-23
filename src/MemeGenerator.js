6b79036bde95ed73620665742fc9748542535efe
Sean McDonald <spmcdonald1316@gmail.com>
2020-09-22 20:28:25 -0500

    render meme display area

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -50,6 +50,11 @@ class MemeGenerator extends React.Component {
           
           <button>Generate Meme</button>
         </form>
+        <div className="meme">
+          <img src={this.state.randomImgSrc} alt="Random Meme" />
+          <h2 className="top">{this.state.topText}</h2>
+          <h2 className="bottom">{this.state.bottomText}</h2>
+        </div>
       </div>
     )
   }
